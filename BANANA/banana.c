e0b59a0de0cccf45a781d38ee382893c67b2c079
Seung-Hyun Shin <riamoney92@gmail.com>
2017-07-21 16:39:15 +0900

    update banana -170721

--- BANANA/banana.c
+++ BANANA/banana.c
@@ -7,6 +7,8 @@
 #include <sys/socket.h>
 
 #define BUFF_SIZE 10
+#define APPLE_D_PORT 1500
+#define APPLE_M_PORT 1509
 
 int main(int argc, char **argv) {
 	int sock;
@@ -14,31 +16,39 @@ int main(int argc, char **argv) {
 
     struct sockaddr_in server_addr;
 
-    char message_send[BUFF_SIZE];
-    char buff_rcv[BUFF_SIZE];
+    char *dummy = "I'm dummy^^";
+    char message[BUFF_SIZE];
+    char apple_ip[BUFF_SIZE];
 
     sock = socket(PF_INET, SOCK_DGRAM, 0);
 
     if(-1 == sock) {
         perror("sock error");
-        exit( 1);
+        exit(1);
     }
 
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family      = AF_INET;
-//  server_addr.sin_port        = htons(4000);
+    server_addr.sin_port        = htons(APPLE_D_PORT);
     server_addr.sin_addr.s_addr = inet_addr("52.78.214.70");
 
-    sprintf(message_send, "%d%c", 0x01, 'B');
+    if(sendto(sock, dummy, strlen(message), 0,
+            (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("sendto error!");
+        exit(1);
+    }
+
+    server_addr_size  = sizeof(server_addr);
+    if(recvfrom(sock, apple_ip, BUFF_SIZE, 0,
+            (struct sockaddr*)&server_addr, &server_addr_size) < 0) {
+        perror("recvfrom error!");
+        exit(1);
+    }
+
+    printf("receive_ip: %s \n", apple_ip);
+
+//  sprintf(message_send, "%d%c", 0x01, 'B');
 //  printf("message : %s  size : %ld\n", message_send, strlen(message_send));
-
-    sendto(sock, message_send, strlen(message_send), 0,
-            (struct sockaddr*)&server_addr, sizeof(server_addr));
-
-//  server_addr_size  = sizeof(server_addr);
-//  recvfrom( sock, buff_rcv, BUFF_SIZE, 0 ,
-//            ( struct sockaddr*)&server_addr, &server_addr_size);
-//  printf("receive: %s \n", buff_rcv);
 
     close(sock);
 
