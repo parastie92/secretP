b33fd53e7aa765c2278c3ae43d3685c3d9641763
Seung-Hyun Shin <riamoney92@gmail.com>
2017-07-21 15:48:47 +0900

    shin banana3

--- banana.c
+++ banana.c
@@ -6,38 +6,41 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 
-#define  BUFF_SIZE   1024
+#define BUFF_SIZE 10
 
-int main( int argc, char **argv) {
+int main(int argc, char **argv) {
 	int sock;
     int server_addr_size;
 
     struct sockaddr_in server_addr;
 
-    char buff_rcv[BUFF_SIZE+5];
+    char message_send[BUFF_SIZE];
+    char buff_rcv[BUFF_SIZE];
 
     sock = socket(PF_INET, SOCK_DGRAM, 0);
 
-    if(-1 == sock)  {
+    if(-1 == sock) {
         perror("sock error");
         exit( 1);
     }
 
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family      = AF_INET;
-    //server_addr.sin_port        = htons(4000);
+//  server_addr.sin_port        = htons(4000);
     server_addr.sin_addr.s_addr = inet_addr("52.78.214.70");
 
+    sprintf(message_send, "%d%c", 0x01, 'B');
+//  printf("message : %s  size : %ld\n", message_send, strlen(message_send));
 
-  //  sendto(sock, argv[1], strlen( argv[1])+1, 0,    // +1: NULL까지 포함해서 전송
-   //          ( struct sockaddr*)&server_addr, sizeof( server_addr));
+    sendto(sock, message_send, strlen(message_send), 0,
+            (struct sockaddr*)&server_addr, sizeof(server_addr));
 
-//    server_addr_size  = sizeof(server_addr);
- //   recvfrom( sock, buff_rcv, BUFF_SIZE, 0 ,
- //            ( struct sockaddr*)&server_addr, &server_addr_size);
-  //  printf("receive: %s \n", buff_rcv);
+//  server_addr_size  = sizeof(server_addr);
+//  recvfrom( sock, buff_rcv, BUFF_SIZE, 0 ,
+//            ( struct sockaddr*)&server_addr, &server_addr_size);
+//  printf("receive: %s \n", buff_rcv);
 
     close(sock);
-   
+
     return 0;
 }
